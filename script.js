2db57b072bc9ca7be3ad5d05e37d4f1d6f7fc4f7
Fran <98226655+P4NCH1T0@users.noreply.github.com>
2024-08-01 16:21:59 -0600

    Snake 2

---
+++ script.js
@@ -0,0 +1,19 @@
+const playBoard = document.querySelector(".play-board");
+
+let foodX , foodY;
+let snakeX, snakeY;
+
+const changefoodposition = () => {
+    foodX = Math.floor(Math.random() * 30) + 1;
+    foodY = Math.floor(Math.random() * 30) + 1;
+}
+
+const initGame = () => {
+    let htmlMarkup = `<div class = "food" style ="grid-area: ${foodY} / ${foodX}"></div>`;
+    htmlMarkup += `<div class = "head" style ="grid-area: ${snakeY} / ${snakeX}"></div>`;
+    playBoard.innerHTML = htmlMarkup;
+
+}
+
+changefoodposition();
+initGame();
