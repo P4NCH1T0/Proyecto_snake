8ae278c50d0675355ebc4c58f4bc34a0961bd32d
P4NCH1T0 <98226655+P4NCH1T0@users.noreply.github.com>
2024-08-01 23:08:10 -0600

    Limites del mapa

--- script.js
+++ script.js
@@ -1,36 +1,45 @@
 const playBoard = document.querySelector(".play-board");
 
+let gameover =  false;
 let foodX , foodY;
 let snakeX =5, snakeY=10;
 let snakeBody = [];
 let velocityX = 0 , velocityY = 0;
-
+let setIntervalID
 
 const changefoodposition = () => {
     foodX = Math.floor(Math.random() * 30) + 1;
     foodY = Math.floor(Math.random() * 30) + 1;
 }
 
+const handleGameOver = () => {
+    clearInterval(setIntervalID);
+    alert("Game Over");
+    location.reload();
+}
+
 const changeDirection = (e) => {
-    if(e.key === "ArrowUp"){
+    if(e.key === "ArrowUp" && velocityY != 1 ){
        velocityX = 0;
        velocityY = -1; 
     }
-    else if(e.key === "ArrowDown"){
+    else if(e.key === "ArrowDown" && velocityY != -1 ){
         velocityX = 0;
         velocityY = 1; 
     }
-    else if(e.key === "ArrowLeft"){
+    else if(e.key === "ArrowLeft" && velocityX != 1 ){
         velocityX = -1;
         velocityY = 0; 
     }
-    else if(e.key === "ArrowRight"){
+    else if(e.key === "ArrowRight" && velocityX != -1 ){
         velocityX = 1;
         velocityY = 0; 
     }
     //initGame();
 }
 const initGame = () => {
+    if(gameover) return handleGameOver();
+
     let htmlMarkup = `<div class = "food" style ="grid-area: ${foodY} / ${foodX}"></div>`;
 
     if(snakeX === foodX && snakeY === foodY){
@@ -46,6 +55,11 @@ const initGame = () => {
 
     snakeX += velocityX;
     snakeY += velocityY;
+
+    if(snakeX <= 0 || snakeX > 30 || snakeY <= 0 || snakeY > 30 ){
+        gameover =  true
+    }
+
     for(let i=0; i < snakeBody.length; i++){
         htmlMarkup += `<div class = "head" style ="grid-area: ${snakeBody[i][1]} / ${snakeBody[i][0]}"></div>`;
     }
@@ -55,6 +69,6 @@ const initGame = () => {
 }
 
 changefoodposition();
-setInterval(initGame, 125);
+setIntervalID = setInterval(initGame, 125);
 
 document.addEventListener("keydown", changeDirection);
